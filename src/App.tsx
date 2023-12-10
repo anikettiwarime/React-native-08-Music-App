1f91ccd7a022075344ff1ba110c60f72dd90b255
Aniket Tiwari <anikettiwari1570@gmail.com>
2023-12-11 03:59:32 +0530

    Updating code base but bug present in library for player state in controll center component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,52 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-const App = (): React.JSX.Element => {
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+import {addTrack, setupPlayer} from '../musicPlayerService';
+import MusicPlayer from './screens/MusicPlayer';
+
+const App = () => {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  const setup = async () => {
+    let isSetup = await setupPlayer();
+
+    if (isSetup) {
+      await addTrack();
+    }
+
+    setIsPlayerReady(isSetup);
+  };
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <StatusBar barStyle={'light-content'} />
+      <MusicPlayer />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
